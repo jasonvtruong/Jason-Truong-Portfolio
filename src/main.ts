31befad80109b58aa70ea9d5dcb81997cfc481de
Jason Truong <W0869805@myscc.ca>
2025-11-09 17:42:52 -0500

    Created skeleton for main page template Generated components for main page, projects, about, and contacts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'}
+];
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
