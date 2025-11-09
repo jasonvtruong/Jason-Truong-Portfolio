4d3eeb01c35c958f80ccb482dbf027fce1d775aa
Jason Truong <W0869805@myscc.ca>
2025-11-09 18:17:16 -0500

    Added routers - Set routes in main.ts for all pages - Eagerly load homepage and projects, lazy load the rest - Moved main page content to its appropriate component

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import {Routes} from '@angular/router';
+import {provideRouter, Routes} from '@angular/router';
+import {MainPage} from './app/main-page/main-page';
+import {PageNotFound} from './app/page-not-found/page-not-found';
+import {ProjectList} from './app/project-list/project-list';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/', pathMatch: 'full'}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: MainPage},    // eagerly loaded
+  {path: 'projects', component: ProjectList},
+  {path: 'about',     // lazy loaded
+    loadComponent: () =>
+      import('./app/about-page/about-page').then(m => m.AboutPage)},
+  {path: 'contact',
+    loadComponent: () =>
+      import('./app/contact-page/contact-page').then(m => m.ContactPage)},
+  {path: '**', // Wildcard route
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found').then(m => m.PageNotFound)}
 ];
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log("Bootstrap successful"))
   .catch((err) => console.error(err));
